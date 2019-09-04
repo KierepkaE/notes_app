dcb73a1b40a1fe142fa8e2f0c272e25d131b1c75
Guillaume <liard.guillaume@gmail.com>
2019-09-04 15:49:45 +0100

    Implemented trimmedTitle method

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -3,8 +3,12 @@ let content = "Note content, nice content";
 
 let note = new Note(title, content);
 
+console.log("#a note has a title and a content")
+
 console.log(expect(note.title).toEqual(title));
 console.log(expect(note.content).toEqual(content));
+
+console.log("#trimmedTitle");
 console.log(expect(note.trimmedTitle()).toEqual(title));
 
 var title2 = "This a title which has 20 characters";
