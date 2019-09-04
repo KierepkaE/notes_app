d53b29a03c7985185fea6036dcb872c31d00635c
kierepkae <kierepkaedyta@gmail.com>
2019-09-04 14:40:32 +0100

    user can add title for the note and click on the title to see full note

--- interface.js
+++ interface.js
@@ -1,13 +1,21 @@
 let notes = [];
+let titles = []
 
 const create_button = document.getElementById('add_note_button')
 const note_value = document.getElementById('note_input')
 const note_list = document.getElementById('note_list')
 const display = document.getElementById('display')
+const title_input = document.getElementById('title_input')
 
-const create_btn_click = () => {
+const createBtnClick = () => {
+    if (title_input.value === '' || note_value.value === '') {
+        throw new Error('This fields can not be empty')
+        return
+    }
     notes.push(note_value.value);
+    titles.push(title_input.value);
     note_value.value = '';
+    title_input.value = '';
     createListItem();
 }
 
@@ -21,8 +29,8 @@ const createListItem = () => {
     clear_list();
     notes.map((note, index) => {
         let new_note = document.createElement('LI');
-        new_note.textContent = trim(note);
-        new_note.setAttribute("class", "ListItem");
+        new_note.textContent = titles[index];
+        new_note.setAttribute("class", "list-item");
         new_note.setAttribute("id", `${index}`);
         new_note.setAttribute("onclick", `displayNote(${index})`);
         note_list.appendChild(new_note);
@@ -34,11 +42,17 @@ const clear_list = () => {
 }
 
 const displayNote = (index) => {
-  display.innerHTML = ''
-  content = notes[index].split("\n");
-  for(line of content) {
-    paragraph = document.createElement('P');
-    paragraph.innerHTML = line;
-    display.appendChild(paragraph);
-  }
+    display.innerHTML = ''
+    content = notes[index].split("\n");
+    let i = 0;
+    for (line of content) {
+        let title_text = titles[i];
+        let title = document.createElement('H4')
+        title.textContent = title_text
+        paragraph = document.createElement('P');
+        paragraph.innerHTML = line;
+        display.appendChild(title)
+        display.appendChild(paragraph);
+        i++;
+    }
 }
