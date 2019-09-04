60de1280555f6b1324c88fba7d5d775d63607bd9
sumeet978 <searchsumeet@gmail.com>
2019-09-04 14:53:54 +0100

    Created Notes class to pass the first test

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -3,5 +3,5 @@ let content = "Note content, nice content";
 
 let note = new Note(title, content);
 
-expect(note.title).toEqual(title);
-expect(note.content).toEqual(content);
+console.log(expect(note.title).toEqual(title));
+console.log(expect(note.content).toEqual(content));
